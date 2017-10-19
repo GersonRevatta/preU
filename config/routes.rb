46ac61ea2d7de7d1e5aa565d137fae018b3bbfbc
gerson <gersonrevatta@gmail.com>
2017-10-19 00:24:52 -0500

    Firt commit pre

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
+
   mount Ckeditor::Engine => '/ckeditor'
   resources :category do 
-    resources :theme
-    resources :level
+    resources :theme do
+      resources :question
+    end
+    resources :level do 
+      resources :question
+    end
   end
   root 'category#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
